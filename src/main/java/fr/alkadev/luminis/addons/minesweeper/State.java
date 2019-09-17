a21363332ea7e9a0627257257114c628a0ddb771
Kaeios <kaeios.contact@gmail.com>
2019-09-17 18:14:46 +0200

    Added method with better performances to get state from bomb count

--- src/main/java/fr/alkadev/luminis/addons/minesweeper/State.java
+++ src/main/java/fr/alkadev/luminis/addons/minesweeper/State.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 
 public enum State {
 
-    BOMB(-1,"||:bomb:|| "),
     ZERO(0, "||:zero:|| "),
     ONE(1, "||:one:|| "),
     TWO(2, "||:two:|| "),
@@ -13,7 +12,8 @@ public enum State {
     FIVE(5, "||:five:|| "),
     SIX(6, "||:six:|| "),
     SEVEN(7, "||:seven:|| "),
-    EIGHT(8, "||:eight:|| ");
+    EIGHT(8, "||:eight:|| "),
+    BOMB(-1,"||:bomb:|| ");
 
     private final int id;
     private final String emote;
@@ -23,11 +23,18 @@ public enum State {
         this.emote = emote;
     }
 
+    // This method has a bad impact on performances use fromBombCount() instead
+    @Deprecated
     public static State from(int id) {
         return Arrays.stream(values())
                 .filter(state -> state.id == id)
                 .findAny()
                 .orElse(State.BOMB);
+    }
+
+    public static State fromBombCount(int ordinal) {
+        final State[] values = values();
+        return (ordinal > 0 && ordinal < values.length) ? values[ordinal] : BOMB;
     }
 
     @Override
